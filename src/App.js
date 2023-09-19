e1588eccd89e765a52c18f768b19296277e63f88
AndrewKetsko <114005173+AndrewKetsko@users.noreply.github.com>
2023-09-19 13:47:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/favorites" element={<CatalogPage />} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="favorites" element={<CatalogPage />} />
       </Route>
       <Route path="*" element={<HomePage />} />
     </Routes>
